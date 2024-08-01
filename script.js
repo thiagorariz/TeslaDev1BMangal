1e15ca8962edd1a18ccbe20146c6d10017199be6
thiagorariz <thiagorariz@gmail.com>
2024-08-01 18:50:14 +0000

    Projeto Tesla

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario =
+    document.querySelector(".formulario");
+//função para aparecer o form
+function aparecerFormulario(){
+    formulario.style.left="50%";
+}
+//função para desaparecer o form
+function desaparecerFormulario(){
+    formulario.style.left="-50%";
+}
